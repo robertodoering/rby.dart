67d957b41170ac3b45395012262ff642f4b7189f
Roberto Doering <rbydoering@gmail.com>
2023-09-24 22:35:17 +0200

    Add sliver gap

--- lib/widgets/gap.dart
+++ lib/widgets/gap.dart
@@ -99,3 +99,80 @@ class Gap extends StatelessWidget {
     );
   }
 }
+
+class SliverGap extends StatelessWidget {
+  const SliverGap(
+    this.mainAxisExtent, {
+    super.key,
+    this.color,
+  }) : _size = _GapSize.custom;
+
+  const SliverGap.xss({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.xxs,
+        mainAxisExtent = null;
+
+  const SliverGap.xs({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.xs,
+        mainAxisExtent = null;
+
+  const SliverGap.s({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.s,
+        mainAxisExtent = null;
+
+  const SliverGap.m({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.m,
+        mainAxisExtent = null;
+
+  const SliverGap.l({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.l,
+        mainAxisExtent = null;
+
+  const SliverGap.xl({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.xl,
+        mainAxisExtent = null;
+
+  const SliverGap.xxl({
+    super.key,
+    this.color,
+  })  : _size = _GapSize.xxl,
+        mainAxisExtent = null;
+
+  final _GapSize _size;
+  final double? mainAxisExtent;
+  final Color? color;
+
+  double _resolveMainAxisExtent(ThemeData theme) {
+    return switch (_size) {
+      _GapSize.custom => mainAxisExtent!,
+      _GapSize.xxs => theme.spacingScheme.xxs,
+      _GapSize.xs => theme.spacingScheme.xs,
+      _GapSize.s => theme.spacingScheme.s,
+      _GapSize.m => theme.spacingScheme.m,
+      _GapSize.l => theme.spacingScheme.l,
+      _GapSize.xl => theme.spacingScheme.xl,
+      _GapSize.xxl => theme.spacingScheme.xxl,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return gap.SliverGap(
+      _resolveMainAxisExtent(theme),
+      color: color,
+    );
+  }
+}
